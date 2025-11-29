bb2639b9710eee671aa6ebbd61ba423e47578dec
Javier Ribó <elribonazo@gmail.com>
2025-11-29 18:27:30 +0100

    fix: change build script Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 cargo build -p kernel --target riscv64gc-unknown-none-elf --release
 cargo build -p relay --release
 cargo build -p riscv-vm --release
-cargo run -p mkfs -- --output target/riscv64gc-unknown-none-elf/release/disk.img --dir mkfs/root --size 64
+cargo run -p mkfs -- --output target/riscv64gc-unknown-none-elf/release/fs.img --dir mkfs/root --size 64
 
 cd riscv-vm && yarn build && cd ..
