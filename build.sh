1c3dd7ae76a520425567d32e61ac68cad5f6515b
Javier Ribó <elribonazo@gmail.com>
2025-12-05 19:17:12 +0100

    fix: lower default kernel image size Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -e
 cargo build -p kernel --target riscv64gc-unknown-none-elf --release
 cargo build -p relay --release
 cargo build -p riscv-vm --release
-cargo run -p mkfs -- --output target/riscv64gc-unknown-none-elf/release/fs.img --dir mkfs/root --size 64
+cargo run -p mkfs -- --output target/riscv64gc-unknown-none-elf/release/fs.img --dir mkfs/root --size 2
 
 cd riscv-vm && yarn build && cd ..
